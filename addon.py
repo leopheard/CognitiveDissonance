4f9f772b47486c42d6b87337d5124fd29a6b911a
leopheard <leopheard.misc@gmail.com>
2019-10-25 22:28:23 -0400

    upload

---
+++ addon.py
@@ -0,0 +1,33 @@
+from xbmcswift2 import Plugin, xbmcgui
+from resources.lib import mainaddon
+
+plugin = Plugin()
+url1 = "https://dissonancepod.com/feed/podcast/"
+@plugin.route('/')
+def main_menu():
+    items = [
+        {
+            'label': plugin.get_string(30001), 
+            'path': plugin.url_for('episodes1'),
+            'thumbnail': "https://dissonancepod.com/wp-content/uploads/powerpress/cd_logo_itunes.jpg"},
+        {
+            'label': plugin.get_string(30000),
+            'path': plugin.url_for('episodes'),
+            'thumbnail': "https://dissonancepod.com/wp-content/uploads/powerpress/cd_logo_itunes.jpg"},
+    ]
+    return items
+
+@plugin.route('/episodes1/'
+def episodes1():
+    soup1 = mainaddon.get_soup1(url1)
+    playable_podcast1 = mainaddon.get_playable_podcast1(soup1)
+    items = mainaddon.compile_playable_podcast1(playable_podcast1)
+    return items
+@plugin.route('/episodes/')
+def episodes():
+    soup1 = mainaddon.get_soup1(url1)
+    playable_podcast = mainaddon.get_playable_podcast(soup1)
+    items = mainaddon.compile_playable_podcast(playable_podcast)
+    return items
+if __name__ == '__main__':
+    plugin.run()
